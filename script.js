373660f0550dd672eb1a4e83fd52c1adc2b4a281
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-01-04 00:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function str(s) {
- //s.toUpperCase()
-	return s.toUpperCase()	
+	var s=document.getElementById("fname").value;
+	return s.toUpperCase();	
 }
 const s = prompt("Enter String:");
 alert(str(s));
